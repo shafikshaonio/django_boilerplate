92ab00af4ead5bc7c50c8c7946dec37e3d9df38e
Mohd. Shafikur Rahman <shafikshaon@gmail.com>
2019-01-14 21:55:44 +0600

    management command to rename project updated

--- core/management/command/rename_project.py
+++ core/management/command/rename_project.py
@@ -7,19 +7,21 @@ class Command(BaseCommand):
     help = 'Renames a Django project'
 
     def add_arguments(self, parser):
+        parser.add_argument('old_project_name', type=str, help='The old project name')
         parser.add_argument('new_project_name', type=str, help='The new project name')
 
     def handle(self, *args, **kwargs):
+        old_project_name = kwargs['old_project_name']
         new_project_name = kwargs['new_project_name']
 
-        files_to_rename = ['django_boilerplate/settings/base.py', 'django_boilerplate/wsgi.py', 'manage.py']
-        folder_to_rename = 'django_boilerplate'
+        files_to_rename = [old_project_name + '/settings/base.py', old_project_name + '/wsgi.py', 'manage.py']
+        folder_to_rename = old_project_name
 
         for f in files_to_rename:
             with open(f, 'r') as file:
                 filedata = file.read()
 
-            filedata = filedata.replace('django_boilerplate', new_project_name)
+            filedata = filedata.replace(old_project_name, new_project_name)
 
             with open(f, 'w') as file:
                 file.write(filedata)
