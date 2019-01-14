80014ba3f3cabd77888399862fa12c452dc49b6a
Mohd. Shafikur Rahman <shafikshaon@gmail.com>
2019-01-14 23:40:38 +0600

    TimeLog, Status abstract models added

--- core/models.py
+++ core/models.py
@@ -1,1 +1,22 @@
-# Create your models here.
+from django.db import models
+
+
+class TimeLog(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
+    deleted_at = models.DateTimeField(null=True, blank=True, editable=False)
+    activated_at = models.DateTimeField(null=True, blank=True, editable=False)
+    deactivated_at = models.DateTimeField(null=True, blank=True, editable=False)
+    published_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Status(models.Model):
+    is_active = models.BooleanField(default=True, null=False, blank=False)
+    is_published = models.BooleanField(default=True, null=False, blank=False)
+    is_deleted = models.BooleanField(default=False, null=False, blank=False)
+
+    class Meta:
+        abstract = True
